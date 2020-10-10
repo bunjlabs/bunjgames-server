297f273e3fbb29d1e5ac21d6f3c78bc5b0513b70
Artem Samokhin <bunjdo@gmail.com>
2020-10-10 22:39:10 +1000

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Sleeping for 5 seconds…"
 sleep 5
 
 echo "Migrate database"
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 echo "Run server"
 daphne -b 0.0.0.0 -p 8000 bunjgames_server.asgi:application
